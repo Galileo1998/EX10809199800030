a918838dcffc206bb1cbf9eb1fa23ddab607cc4f
Galileo <galileoga63@gmail.com>
2019-10-15 18:26:55 -0600

    Tercer Commit Post
    Creación de Post

--- routes/api/fotografiasGaleria/index.js
+++ routes/api/fotografiasGaleria/index.js
@@ -16,7 +16,42 @@ router.get('/', function (req, res) {
 router.get('/all', function(req, res){
     conCollection = fileModel.getFotografias();
     res.json(conCollection);
-  }); // get /all
+  }); 
+
+router.post('/new', function(req, res){
+    fotoCollection = fileModel.getFotografias();
+    var newFotografias = Object.assign(
+       {},
+       req.body,
+       {
+           "title": req.body.title,
+           "url": req.body.url,
+           "thumbnailUrl": req.body.thumbnailUrl,
+           "album": req.body.album
+       }
+    );
+    var fotografiasExists = fotoCollection.find(
+      function(o, i){
+        return o.id === newFotografias.id;
+      }
+    )
+    if( ! fotografiasExists ){
+      fotoCollection.push(newFotografias);
+      fileModel.setFotografias(
+         fotoCollection,
+         function(err, savedSuccesfully){
+           if(err){
+             res.status(400).json({ "error": "No se pudo ingresar objeto" });
+           } else {
+             res.json(newFotografias);  // req.body ===  $_POST[]
+           }
+         }
+       );
+    } else {
+      res.status(400).json({"error":"No se pudo ingresar objeto"});
+    }
+ }); // post /new
+
 
 
 module.exports = router;
